0f0349b259bb69cf5dfb852cb2237da661443429
aegis <aegis>
2002-06-05 07:36:09 +0000

    oops, forgot to escape some ampersands in URLs

--- web/development.php
+++ web/development.php
@@ -8,8 +8,8 @@ Corona development primarily takes place on
 <a href="http://sf.net/">SourceForge</a>.  The
 <a href="http://sf.net/projects/corona/">project page</a> has
 <a href="http://sourceforge.net/project/showfiles.php?group_id=44336">downloads</a>,
-<a href="http://sourceforge.net/tracker/?atid=439230&group_id=44336&func=browse">bug reports</a>,
-<a href="http://sourceforge.net/tracker/?atid=439233&group_id=44336&func=browse">feature requests</a>,
+<a href="http://sourceforge.net/tracker/?atid=439230&amp;group_id=44336&amp;func=browse">bug reports</a>,
+<a href="http://sourceforge.net/tracker/?atid=439233&amp;group_id=44336&amp;func=browse">feature requests</a>,
 <a href="http://sourceforge.net/mail/?group_id=44336">mailing lists</a>, a
 <a href="http://sourceforge.net/pm/?group_id=44336">task manager</a>, and a web-based
 <a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/corona">CVS viewer</a>.
