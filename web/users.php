7b1b25858958d33fad2d6efa5d113afdf9ed0c2d
aegis <aegis>
2003-05-25 01:30:00 +0000

    adding VR Juggler toolbox to users page

--- web/users.php
+++ web/users.php
@@ -14,6 +14,7 @@ Projects that use Corona are listed here.
 <li><a href="http://aegisknight.org/sphere">Sphere</a></li>
 <li><a href="http://www.verge-rpg.com/">VERGE 3</a></li>
 <li><a href="http://virtualdubmod.sourceforge.net/">VirtualDubMod</a></li>
+<li><a href="http://vrjtoolbox.sourceforge.net/">VR Juggler Toolbox</a></li>
 </ul>
 
 <p>
