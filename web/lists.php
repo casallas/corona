0dc6966269222ceaf7fdf0861500652c30485c26
aegis <aegis>
2004-03-19 07:26:59 +0000

    fix links to mailing list archives

--- web/lists.php
+++ web/lists.php
@@ -2,11 +2,11 @@
 $current_page = 'lists';
 include 'page_header.inc';
 
-function mailing_list($name, $description) {
+function mailing_list($name, $id, $description) {
   $mailto    = 'mailto:' . $name . '@lists.sf.net';
   $subscribe = 'http://lists.sourceforge.net/lists/listinfo/' . $name;
-  $archive   = 'http://www.geocrawler.com/redir-sf.php3?list=' . $name;
-    
+  $archive   = 'http://sourceforge.net/mailarchive/forum.php?forum_id=' . $id;
+
   echo '<tr>';
   echo "<td><a href=\"$mailto\">$name</a></td>";
   echo '<td>';
@@ -18,15 +18,15 @@ function mailing_list($name, $description) {
 }
 
 echo '<table>';
-mailing_list('corona-annc', 'Announcements and news related to Corona.  ' .
+mailing_list('corona-annc', '7924', 'Announcements and news related to Corona.  ' .
              'This is a low-volume list for those who are only interested in ' .
              'new releases.');
-mailing_list('corona-users', 'User information and discussion.  Use this list ' .
+mailing_list('corona-users', '8226', 'User information and discussion.  Use this list ' .
              'to get in contact with other Corona users.');
-mailing_list('corona-devel', 'Developer discussion.  This list is for discussion ' .
+mailing_list('corona-devel', '7943', 'Developer discussion.  This list is for discussion ' .
              'about the development process and other topics the average user ' .
              'does not care about.  Use this list to reach the developers.');
-mailing_list('corona-commits', 'Relatively high-volume list that logs all CVS ' .
+mailing_list('corona-commits', '7678', 'Relatively high-volume list that logs all CVS ' .
              'commits.  Subscribe to this list if you want to see every change ' .
              'to the source code when they happen.');
 echo '</table>';
